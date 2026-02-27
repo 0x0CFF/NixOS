2dab260bc3468ad9c346f9c54f51ec7f459ba2e6
0x0CFF <0x0CFF@proton.me>
2026-02-27 21:09:05 +0800

    更新桌面模块

--- NixOS-Flake/Modules/Home/Crates/Desktop/DE/default.nix
+++ NixOS-Flake/Modules/Home/Crates/Desktop/DE/default.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     playerctl                # [CLI][C] 媒体播放器控制
     wev                      # [CLI][C] Wayland 事件查看器
+    tuigreet                 # [TUI][RUST] 登录屏幕
     fuzzel                   # [GUI][C] 应用启动器
     rofi                     # [GUI][C] 应用启动器
     libnotify                # [GUI][C] 通知守护进程
@@ -12,9 +13,9 @@
     hyprpicker               # [GUI][C++] 取色器
     hypridle                 # [GUI][C++] 空闲守护进程
     waybar                   # [GUI][C++] Wayland 桌面栏
-    alacritty                # [GUI][RUST] 终端
     xdg-desktop-portal-gtk   # [GUI][GTK+] 桌面门户
     xdg-desktop-portal-gnome # [GUI][GTK+] 桌面门户
+    alacritty                # [GUI][RUST] 终端
     xwayland-satellite       # [GUI][RUST] xwayland 实现
   ];
   
@@ -30,8 +31,25 @@
     enable = true;           # [GUI][C++] 屏幕锁定程序
   };
   
-  services.displayManager = {
-    autoLogin.enable = true;
-    autoLogin.user = "0x0CFF";
+  # services.displayManager = {
+  #   autoLogin.enable = true;
+  #   autoLogin.user = "0x0CFF";
+  # };
+  
+  services.greetd = {
+    enable = true;
+    settings = {
+     default_session.command = ''
+      ${pkgs.greetd.tuigreet}/bin/tuigreet \
+        --time \
+        --asterisks \
+        --user-menu \
+        --cmd niri
+    '';
+    };
   };
+
+  environment.etc."greetd/environments".text = ''
+    niri
+  '';
 }
