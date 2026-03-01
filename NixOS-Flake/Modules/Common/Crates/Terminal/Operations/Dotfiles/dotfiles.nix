eebf00190f4108dd0ad076110cd43ff9e16a42db
0x0CFF <0x0CFF@proton.me>
2026-03-01 14:38:44 +0800

    修复语法错误

--- NixOS-Flake/Modules/Common/Crates/Terminal/Operations/Dotfiles/dotfiles.nix
+++ NixOS-Flake/Modules/Common/Crates/Terminal/Operations/Dotfiles/dotfiles.nix
@@ -1,14 +1,17 @@
 { config, pkgs, inputs, ... }:let
   # 配置目录路径
   # ${config.home.homeDirectory} 表示 Home-Manager 管理的用户家目录
-  bottomPath = "${config.home.homeDirectory}/Solution/Blueprints/NixOS/NixOS-Flake/Modules/Common/Crates/Terminal/Operations/Dotfiles/Bottom";
-  naviPath = "${config.home.homeDirectory}/Solution/Blueprints/NixOS/NixOS-Flake/Modules/Common/Crates/Terminal/Operations/Dotfiles/Navi";
+  bottomConfigPath = "${config.home.homeDirectory}/Solution/Blueprints/NixOS/NixOS-Flake/Modules/Common/Crates/Terminal/Operations/Dotfiles/Bottom";
+  naviConfigPath = "${config.home.homeDirectory}/Solution/Blueprints/NixOS/NixOS-Flake/Modules/Common/Crates/Terminal/Operations/Dotfiles/Navi";
 in
 {
   # config.lib.file.mkOutOfStoreSymlink 可以创建一个指向 Dotfiles 绝对路径的软链接
   # 该函数能够绕过 Home Manager 自身，对 Dotfiles 的修改就能立即生效
   # 注意：仅支持原生配置，由 Nix 生成的配置不支持
-  xdg.configFile."bottom".source = config.lib.file.mkOutOfStoreSymlink bottomPath;
-  xdg.configFile."navi".source = config.lib.file.mkOutOfStoreSymlink naviPath;
+  # xdg.dataFile -> ~/.local/share/
+  # xdg.configFile -> ~/.config/
+  # 拓展：https://wiki.archlinux.org/title/XDG_Base_Directory
+  xdg.configFile."bottom".source = config.lib.file.mkOutOfStoreSymlink bottomConfigPath;
+  xdg.configFile."navi".source = config.lib.file.mkOutOfStoreSymlink naviConfigPath;
   xdg.configFile."starship.toml".source = ./Starship/starship.toml;
 }
