8411e1f1072cc908231e035674a7583bb8c2876d
0x0CFF <0x0CFF@proton.me>
2026-03-02 21:46:09 +0800

    修改桌面组件

--- NixOS-Flake/Modules/Home/Crates/Desktop/Recreation/default.nix
+++ NixOS-Flake/Modules/Home/Crates/Desktop/Recreation/default.nix
@@ -3,6 +3,6 @@
 {
   environment.systemPackages = with pkgs; [
     jellyfin                 # [GUI][C#] 视频流媒体播放器
-    sonixd                   # [GUI][TypeScript] 音乐播放器
+    feishin                  # [GUI][TypeScript] 音乐播放器
   ];
 }
