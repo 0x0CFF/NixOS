6c1632b9235e03d0e1eba5c1ab262f8e435118ce
0x0CFF <0x0CFF@proton.me>
2026-03-07 21:03:17 +0800

    添加桌面组件

--- NixOS-Flake/Modules/Home/Crates/Desktop/Produce/default.nix
+++ NixOS-Flake/Modules/Home/Crates/Desktop/Produce/default.nix
@@ -5,7 +5,8 @@
     # darktable                # [GUI][C] 摄影工作流程应用
     # obs-studio               # [GUI][C] 直播录屏应用
     # blender                  # [GUI][C++] 3D 创作套件
-    # krita                    # [GUI][C++] 数字绘画应用
+    krita                    # [GUI][C++] 数字绘画应用
+    wpsoffice-cn             # [GUI][C++] Office 办公套件
     obsidian                 # [GUI][JAVASCRIPT] 笔记管理应用
     # gyroflow                 # [GUI][RUST] 视频稳定应用
     zed-editor               # [GUI][RUST] 代码编辑器
