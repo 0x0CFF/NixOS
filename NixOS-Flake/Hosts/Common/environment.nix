c4bdffa0267bd1096fc38c9d0106d8a351b47e43
0x0CFF <0x0CFF@proton.me>
2026-03-08 16:29:52 +0800

    修改终端配置

--- NixOS-Flake/Hosts/Common/environment.nix
+++ NixOS-Flake/Hosts/Common/environment.nix
@@ -4,6 +4,7 @@
   # 系统环境变量
   environment.variables = {
     EDITOR = "hx";                # 系统默认编辑器
+    TLDR_LANGUAGE="zh";           # TLDR 手册语言
     # NIXPKGS_ALLOW_BROKEN = 1;     # 允许系统使用破损包
   };
 }
