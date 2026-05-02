9cf7f89c44a54b45995ec01433ec542a45f2c95a
0x0CFF <0x0CFF@proton.me>
2026-05-02 23:18:48 +0800

    更新配置

--- NixOS-Flake/Modules/Home/Services/Produce/Immich/default.nix
+++ NixOS-Flake/Modules/Home/Services/Produce/Immich/default.nix
@@ -1,12 +1,14 @@
 { config, pkgs, inputs, ... }:
 
 {
-  _class = "nixos";
   services.immich = {
     enable = true;
     host = "0.0.0.0";
     port = 2283;
     openFirewall = true;     # 开放防火墙端口
     mediaLocation = "/var/lib/immich";
+  };
+  services.immich.machine-learning = {
+    enable = false;
   };
 }
